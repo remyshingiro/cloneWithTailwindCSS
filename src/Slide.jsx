e41811b4f725f58a5e24f36b5ccbc6ba801de9ad
Remy <shingiroremy303@gmail.com>
2025-11-25 14:41:25 +0200

    .

--- src/Slide.jsx
+++ src/Slide.jsx
@@ -1,6 +1,42 @@
 function Slide(){
     return(
         <>
+            <div className="w-285 h-50 absolute top-166 left-38 rounded-xl bg-white">
+                <div className="w-6 h-6 absolute top-188 left-64">
+                    <img className="w-6 h-6 absolute top-188 left-64" src="public/Group 1216.png"/>
+                </div>
+
+                <div>
+                    <h3>90+</h3>
+                    <h3>Users</h3>
+                </div>
+
+                <div>
+                    <img src=""/>
+                </div>
+
+                <div>
+                    <img src=""/>
+                </div>
+
+                <div>
+                    <h3>30+</h3>
+                    <h3>Locations</h3>
+                </div>
+
+                <div>
+                    <img src=""/>
+                </div>
+
+                <div>
+                    <img src=""/>
+                </div>
+
+                <div>
+                    <h3>50+</h3>
+                    <h3>Servers</h3>
+                </div>
+            </div>
         </>
     )
 }
